affa69f2e2cd5de78190a470752af3fce16a8b57
Uddam Bhathal <Uddamsingh14@gmail.com>
2024-04-26 21:19:15 -0400

    Update openai.js

--- src/openai.js
+++ src/openai.js
@@ -1,7 +1,7 @@
 import OpenAI from 'openai';
 
 // Storing OpenAI API Key
-const openai = new OpenAI({apiKey: REACT_APP_API_KEY, dangerouslyAllowBrowser: true});
+const openai = new OpenAI({apiKey: process.env.REACT_APP_API_KEY, dangerouslyAllowBrowser: true});
 
 // Handling messages sent to API with parameters like model and messages, returning API output
 export async function sendMsgToOpenAI(message){
